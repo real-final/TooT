65f0b048838cfbe0dbdb3633d28b12cf7056e7d9
h2002y <h2002y@naver.com>
2023-09-11 17:20:32 +0900

    S09P22A709-159/feat: 미니 랭킹 자잘한 CSS 변경

--- frontend/src/components/ranking/mini/miniRankingItem.tsx
+++ frontend/src/components/ranking/mini/miniRankingItem.tsx
@@ -8,9 +8,9 @@ const MiniRankingItem = ({user, index}:{user: {
     <tr className="w-full">
       <td align="center" className={`text-[28px] align-middle ${index === 0 ? "text-first " : " "}${index === 1 ? "text-second " : " "}${index === 2 ? "text-third " : " "}`}>{index + 1}</td>
       <td align="center" className="w-[40px] h-[40px] rounded-full bg-point align-middle">{user.profile}</td>
-      <td align="center" className="text-[16px] align-middle">{user.nickname}</td>
-      <td align="right" className={`text-[16px] align-middle ${user.profit > 0 ? "text-stockRed" : "text-stockBlue"}`}>{user.profit}<span className="text-stockGray">원</span></td>
-      <td align="right" className="text-[16px] align-middle text-stockGray">파산 {user.gameover}회</td>
+      <td align="center" className="text-[20px] align-middle">{user.nickname}</td>
+      <td align="right" className={`text-[20px] align-middle ${user.profit > 0 ? "text-stockRed" : "text-stockBlue"}`}>{user.profit.toLocaleString('ko-KR')}<span className="text-stockGray"> 원</span></td>
+      <td align="right" className="text-[14px] align-middle text-stockGray">파산 {user.gameover}회</td>
     </tr>
   );
 };
