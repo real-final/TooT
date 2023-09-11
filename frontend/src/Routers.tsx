e6b33454feb9bfb54c65e98026be602990741745
서재현 <sjh960815@naver.com>
2023-09-11 16:53:45 +0900

    S09P22A709-273/feat: 404 not found 페이지 구현

--- frontend/src/Routers.tsx
+++ frontend/src/Routers.tsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router-dom";
-import Example from "./components/main/Example";
 import Home from "./components/main/Home";
+import NotFound from "./common/notfound/NotFound"
+import Example from "./components/main/Example";
+
 
 const Routers = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/stock" element={<Example />}></Route>
-      <Route path="/user" element={<Example />}></Route>
-      <Route path="/rank" element={<Example />}></Route>
-      <Route path="/quiz" element={<Example />}></Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/stock" element={<Example />} />
+      <Route path="/user" element={<Example />} />
+      <Route path="/rank" element={<Example />} />
+      <Route path="/quiz" element={<Example />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
