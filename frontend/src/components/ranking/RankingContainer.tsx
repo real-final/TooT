cdf21807955eb3d60d0aff25824cb399584c95a8
h2002y <h2002y@naver.com>
2023-09-12 14:17:59 +0900

    S09P22A709-159/feat: 랭킹 트로피 이모티콘 추가

--- frontend/src/components/ranking/RankingContainer.tsx
+++ frontend/src/components/ranking/RankingContainer.tsx
@@ -1,9 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrophy } from "@fortawesome/free-solid-svg-icons";
 import Ranking from "./Ranking";
 
 const RankingContainer = ({ title } : {size: string, title:string}) => {
   return(
     <div className="w-full h-full p-10 min-h-0">
-      <div className="text-[36px]">{title}</div>
+      <div className="text-[36px]">{title} <FontAwesomeIcon className="text-first" icon={faTrophy} /></div>
       <br></br>
       <div className="w-[95%] h-[95%] h-min-0">
         <Ranking size="big" />
