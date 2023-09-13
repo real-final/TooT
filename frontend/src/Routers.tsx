ea1acdf149f1c30a38699f3a0e94c5ada7c33e54
h2002y <h2002y@naver.com>
2023-09-13 12:32:43 +0900

    S09P22A709-180/feat: 유저 주식 조회 추가

--- frontend/src/Routers.tsx
+++ frontend/src/Routers.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import Example from "./components/main/Example";
 import Home from "./components/main/Home";
 import RankingContainer from "./components/ranking/RankingContainer";
+import UserStock from "./components/user/UserStock";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/stock" element={<Example />}></Route>
-      <Route path="/user" element={<Example />}></Route>
+      <Route path="/user" element={<Example />}>
+        <Route path="stock" element={<UserStock />}></Route>
+      </Route>
       <Route path="/rank" element={<Example />}>
         <Route path="total" element={<RankingContainer size="big" title="전체 랭킹" />}></Route>
         <Route path="friend" element={<RankingContainer size="big" title="친구 랭킹" />}></Route>
--- frontend/src/components/ranking/RankingItem.tsx
+++ frontend/src/components/ranking/RankingItem.tsx
@@ -7,11 +7,11 @@ const RankingItem = ({size, user, index}:{user: {
   size: string}) => {
   return (
     <tr className="w-full">
-      <td align="center" className={`${size === "small" ? "text-[28px]" : "text-[52px]"} align-middle ${index === 0 ? "text-first " : " "}${index === 1 ? "text-second " : " "}${index === 2 ? "text-third " : " "}`}>{index + 1}</td>
+      <td align="center" className={`${size === "small" ? "text-[24px]" : "text-[52px]"} align-middle ${index === 0 ? "text-first " : " "}${index === 1 ? "text-second " : " "}${index === 2 ? "text-third " : " "}`}>{index + 1}</td>
       <td align="center" className={`${size === "small" ? "w-[40px] h-[40px]" : "w-[80px] h-[80px]"} rounded-full bg-point align-middle`}>{user.profile}</td>
-      <td align="center" className={`${size === "small" ? "text-[20px]" : "text-[28px]"} align-middle`}>{user.nickname}</td>
-      <td align="right" className={`${size === "small" ? "text-[20px]" : "text-[28px]"} align-middle ${user.profit > 0 ? "text-stockRed" : "text-stockBlue"}`}>{user.profit.toLocaleString('ko-KR')}<span className="text-stockGray"> 원</span></td>
-      <td align="right" className={`${size === "small" ? "text-[14px]" : "text-[20px]"} align-middle text-stockGray`}>파산 {user.gameover}회</td>
+      <td align="center" className={`${size === "small" ? "text-[16px]" : "text-[28px]"} align-middle`}>{user.nickname}</td>
+      <td align="right" className={`${size === "small" ? "text-[16px]" : "text-[28px]"} align-middle ${user.profit > 0 ? "text-stockRed" : "text-stockBlue"}`}>{user.profit.toLocaleString('ko-KR')}<span className="text-stockGray"> 원</span></td>
+      <td align="right" className={`${size === "small" ? "text-[12px]" : "text-[20px]"} align-middle text-stockGray`}>파산 {user.gameover}회</td>
     </tr>
   );
 };
