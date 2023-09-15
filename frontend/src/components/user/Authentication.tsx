891ddeb2b4a36fbdb7bdcb2d56dd52a296b12c74
서재현 <sjh960815@naver.com>
2023-09-15 16:46:08 +0900

    S09P22A709-293/fix: 주석 추가

--- frontend/src/components/user/Authentication.tsx
+++ frontend/src/components/user/Authentication.tsx
@@ -7,13 +7,15 @@ const useQuery = () => {
   return new URLSearchParams(useLocation().search);
 };
 
+/** Kakao 로그인 후 params에 담긴 code를 서버로 보내주기 */
 const Authentication: React.FC = () => {
   const query = useQuery();
   const code = query.get("code"); // 인증 Code 저장
 
   const fetchData = async () => {
     try {
-      await api.get(`/auth/kakao/callback?code=${code}`);
+      await api.get(`/user/login/kakao?code=${code}`);
+      console.log("서버 전송성공");
       window.location.replace("/");
     } catch (error) {
       console.error(error);
