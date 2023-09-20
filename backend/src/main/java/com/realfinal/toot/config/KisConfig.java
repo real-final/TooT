859b09ff3daaf10bb1c018eecea4d798c05611c5
한다솜 <hmdasom@naver.com>
2023-09-20 16:34:48 +0900

    S09P22A709-383/fix: 무조건 @Data를 써야해서 다시 바꿈

--- backend/src/main/java/com/realfinal/toot/config/KisConfig.java
+++ backend/src/main/java/com/realfinal/toot/config/KisConfig.java
@@ -1,12 +1,12 @@
 package com.realfinal.toot.config;
 
-import lombok.Getter;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "kis")
-@Getter
+@Data
 public class KisConfig {
   private String appKey;
   private String appSecret;
