3cc23ba0d398a71782d6b2f2f7926786f7ad8b3f
이예린 <h2002y@naver.com>
2023-09-26 12:15:21 +0900

    HOTFIX: redux 이식 후 챗봇 재연동

--- frontend/src/utils/chat/chat.ts
+++ frontend/src/utils/chat/chat.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Ibubble } from "../../interface/Ibubble";
 import { add } from "../../store/slices/bubbleSlice";
+import { codeSwitch } from "./codeSwitch";
 
 export const sendBubble = (text:string, dispatch: any) => {
   const newBubble: Ibubble = {
@@ -11,6 +12,9 @@ export const sendBubble = (text:string, dispatch: any) => {
 };
 
 export const getBubble = async (text:string, dispatch: any) => {
+
+  let bubble = "";
+  let bubbleData:any = [];
   
   // TODO: userID와 timestamp 값 동적으로 바꾸기
   const sendData = {
@@ -21,13 +25,31 @@ export const getBubble = async (text:string, dispatch: any) => {
   };
 
   const newBubble: Ibubble = {
+    type: "",
+    url: undefined,
     message: "",
     speaker: false
   };
 
-  // TODO: axios => react query + axios로 바꾸기
   await axios.post("https://clovachatbot.ncloud.com/api/chatbot/messenger/v1/11717/4341b324382837bdd4e3484b0ba438beb6f358968d0a6d09cfcacd9396c11ce6/message", sendData).then(
     (response) => { 
       console.log(response);
-      // TODO: 이미지가 오는 경우 고려(기존 버블 응답 객체와는 속성 이름이 달라서 추가로 수정 필요)
-    dispatch(add(newBubble));})};
+      const data = response.data;
+      const bubbleType = data.content[0].type;
+      newBubble.type = bubbleType;
+      if(bubbleType === "text"){
+        const bubbleSlices = data.content[0].data.details.split('|');
+        bubbleData = bubbleSlices.slice(0, -1);
+        bubble = bubbleSlices.slice(-1);
+        newBubble.message = bubble;
+      }
+      else if(bubbleType === "image"){
+        const bubbleSlices = data.content[0].data.description.split('|');
+        newBubble.url = data.content[0].data.url;
+        bubble = bubbleSlices.slice(-1);
+        newBubble.message = bubble;
+      }
+  });
+  await dispatch(add(newBubble));
+  await codeSwitch(bubble, bubbleData);
+};
