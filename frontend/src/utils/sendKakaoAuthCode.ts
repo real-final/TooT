5eb87be4cccbb4164bfb75748f9a52f189a51953
이예린 <h2002y@naver.com>
2023-10-01 18:54:08 +0900

    Update frontend/src/utils/sendKakaoAuthCode.ts

--- frontend/src/utils/sendKakaoAuthCode.ts
+++ frontend/src/utils/sendKakaoAuthCode.ts
@@ -2,7 +2,7 @@ import { api } from "./api";
 
 export const sendKakaoAuthCode = async (code: string) => {
   try {
-    await api.patch(`/user/login/kakao?code=${code}`);
+    await api.get(`/user/login/kakao?code=${code}`);
     window.location.replace("/"); // 브라우저 URL 안의 parameter를 지우기 위한 새로고침
   } catch (error) {
     console.error("위치: sendKakaoAuthCode.ts, 카카오 OAuth 인증코드 서버 전송 실패");
