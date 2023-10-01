4279508038b10da2694e9df604a8a2771c3f5a45
heejo <heejo515@naver.com>
2023-10-01 15:58:13 +0900

    S09P22A709-480/fix : 챗봇 ibubble 수정

--- backend/src/main/java/com/realfinal/toot/api/chatbot/dto/Ibubble.java
+++ backend/src/main/java/com/realfinal/toot/api/chatbot/dto/Ibubble.java
@@ -1,11 +1,14 @@
 package com.realfinal.toot.api.chatbot.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
+@Builder
 public class Ibubble {
 
     private final String type;
@@ -13,11 +16,15 @@ public class Ibubble {
     private final String url;
     private final Boolean speaker;
 
-    @Builder
-    public Ibubble(String type, String message, String url, Boolean speaker) {
+    @JsonCreator
+    public Ibubble(@JsonProperty("type") String type,
+            @JsonProperty("message") String message,
+            @JsonProperty("url") String url,
+            @JsonProperty("speaker") Boolean speaker) {
         this.type = type;
         this.message = message;
         this.url = url;
         this.speaker = speaker;
     }
 }
+
