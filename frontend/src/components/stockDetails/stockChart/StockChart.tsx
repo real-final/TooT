77fa0ea2062810f18314bb6d9df10a22c1b68baf
서재현 <sjh960815@naver.com>
2023-10-04 17:53:50 +0900

    Add: 분봉, 주봉 구현 완료

--- frontend/src/components/stockDetails/stockChart/StockChart.tsx
+++ frontend/src/components/stockDetails/stockChart/StockChart.tsx
@@ -1,7 +1,6 @@
-import ReactApexChart from "react-apexcharts";
-
 import { IstockItem } from "../../../interface/IstockDetails";
-import { IchartDataItem } from "../../../interface/IstockDetails";
+
+import ApexChart from "./ApexChart";
 
 import Tabs from "@mui/joy/Tabs";
 import TabList from "@mui/joy/TabList";
@@ -10,7 +9,9 @@ import TabPanel from "@mui/joy/TabPanel";
 import Typography from "@mui/joy/Typography";
 
 const StockChart: React.FC<{ stockItem: IstockItem }> = ({ stockItem }) => {
-  let minCandle = stockItem?.minCandle;
+  let minCandle = stockItem?.minCandle; // 분봉
+  let dayCandle = stockItem?.dayCandle; //일봉
+  let weekCandle = stockItem?.weekCandle; //주봉
   return (
     <Tabs
       aria-label="stock-chart"
@@ -52,123 +53,13 @@ const StockChart: React.FC<{ stockItem: IstockItem }> = ({ stockItem }) => {
         <ApexChart title="min" data={minCandle} />
       </TabPanel>
       <TabPanel value={1} sx={{ padding: "0" }} className="no-scrollbar">
-        <ApexChart title="day" data={minCandle} />
+        <ApexChart title="day" data={dayCandle} />
       </TabPanel>
       <TabPanel value={2} sx={{ padding: "0" }} className="no-scrollbar">
-        <ApexChart title="week" data={minCandle} />
+        <ApexChart title="week" data={weekCandle} />
       </TabPanel>
     </Tabs>
   );
 };
 
 export default StockChart;
-
-/** 분봉의 시간 문자열을 파싱하는 함수 */
-const parseMinTime = (timeStr: string) => {
-  const hour = timeStr.substring(0, 2);
-  const minute = timeStr.substring(2, 4);
-  return `${parseInt(hour, 10)}시 ${parseInt(minute, 10)}분`;
-};
-
-/** 주식 차트 UI */
-const ApexChart: React.FC<{ title: string; data: IchartDataItem[] }> = ({
-  title,
-  data,
-}) => {
-  let times = null;
-  if (title === "min") {
-    times = data.map((item) => parseMinTime(item.time)).reverse();
-  } else {
-    times = data.map((item) => item.time).reverse();
-  }
-  const prices = data.map((item) => parseInt(item.price)).reverse();
-  const amounts = data.map((item) => parseInt(item.amount)).reverse();
-
-  const sharedChartOptions = {
-    chart: {
-      group: "stock-charts", // 동일한 그룹 이름으로 두 차트를 그룹화
-      toolbar: {
-        show: true, // Line 그래프의 도구 모듈 활성화
-      },
-    },
-    xaxis: {
-      categories: times,
-    },
-  };
-
-  const priceOptions = {
-    ...sharedChartOptions,
-    chart: {
-      ...sharedChartOptions.chart,
-    },
-    stroke: {
-      curve: "smooth" as const, // 선을 부드럽게 곡선 형태로 연결
-    },
-    grid: {
-      padding: {
-        top: 0,
-        bottom: -25,
-      },
-    },
-    xaxis: {
-      ...sharedChartOptions.xaxis,
-      labels: {
-        show: false, // Line 그래프의 x축 라벨 숨김
-      },
-      axisTicks: {
-        show: false, // Line 그래프의 x축 눈금 숨김
-      },
-    },
-  };
-
-  const amountOptions = {
-    ...sharedChartOptions,
-    chart: {
-      ...sharedChartOptions.chart,
-      toolbar: {
-        show: false, // Bar 그래프의 도구 모듈 비활성화
-      },
-    },
-    grid: {
-      padding: {
-        top: -20,
-        bottom: -5,
-      },
-    },
-    dataLabels: {
-      enabled: true, // 거래량 데이터 값 보여줌
-    },
-    xaxis: {
-      ...sharedChartOptions.xaxis,
-      labels: {
-        show: true,
-        hideOverlappingLabels: true,
-        trim: false,
-        formatter: function (val: string) {
-          const min = parseInt(val.split("시")[1], 10);
-          return !(min % 10) ? val : "";
-        },
-      },
-      axisTicks: {
-        show: true, // x축 눈금 보여줌
-      },
-    },
-  };
-
-  return (
-    <div className="h-full">
-      <ReactApexChart
-        options={priceOptions}
-        series={[{ name: "시가", data: prices }]}
-        height="66%"
-        type="line"
-      />
-      <ReactApexChart
-        options={amountOptions}
-        series={[{ name: "거래량", data: amounts }]}
-        height="34%"
-        type="bar"
-      />
-    </div>
-  );
-};
