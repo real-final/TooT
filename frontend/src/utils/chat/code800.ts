e719e5b84569425b0e75b4213af55ef38832b1b9
이예린 <h2002y@naver.com>
2023-10-04 17:19:26 +0900

    Update frontend/src/utils/chat/code800.ts

--- frontend/src/utils/chat/code800.ts
+++ frontend/src/utils/chat/code800.ts
@@ -2,7 +2,6 @@ import { setRespondingFalse } from "../../store/slices/chatInputSlice";
 
 /* 800: 기타 */
 export const code800 = async (code:number, dispatch:any) => {
-  console.log(code);
   switch(code){
     case 800:
       await dispatch(setRespondingFalse());
