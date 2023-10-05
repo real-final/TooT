70c816997125f2f530e540242a7ab4808ae6eb64
서재현 <sjh960815@naver.com>
2023-10-05 15:31:01 +0900

    Add: 각종 디버깅, 코드 리팩토링

--- frontend/src/components/chat/input/Input.tsx
+++ frontend/src/components/chat/input/Input.tsx
@@ -14,7 +14,9 @@ const Input = () => {
   const navigate = useNavigate();
   const userAuthContext = useContext(UserAuthContext);
 
-  const isChatbotResponding = useSelector((state:RootState) => state.chatbotResponding.isChatbotResponding);
+  const isChatbotResponding = useSelector(
+    (state: RootState) => state.chatbotResponding.isChatbotResponding
+  );
 
   const handleInputTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputText(e.target.value);
@@ -28,21 +30,29 @@ const Input = () => {
   };
 
   const handleKeyDown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if(e.key === "Enter"){
-      if(inputText === "") return;
+    if (e.key === "Enter") {
+      if (inputText === "") return;
       await handleBubble();
     }
   };
 
   const handleSendClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    if(inputText === "") return;
+    if (inputText === "") return;
     await handleBubble();
   };
 
   return (
     <div className="w-full h-9 pl-2.5 pr-1 flex items-center justify-between rounded-full bg-gray-300">
-      <input type="text" disabled={isChatbotResponding} autoFocus value={inputText} onChange={handleInputTextChange} className="w-full h-full font-light text-[14px] bg-gray-300 focus:outline-none ml-1.5" onKeyDown={handleKeyDown} ></input>
+      <input
+        type="text"
+        disabled={isChatbotResponding}
+        autoFocus
+        value={inputText}
+        onChange={handleInputTextChange}
+        className="w-full h-full font-light text-[14px] bg-gray-300 focus:outline-none ml-1.5"
+        onKeyDown={handleKeyDown}
+      ></input>
       <SendButton handleSendClick={handleSendClick} />
     </div>
   );
--- frontend/src/components/tutorials/Tutorials.tsx
+++ frontend/src/components/tutorials/Tutorials.tsx
@@ -1,6 +1,6 @@
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowAltCircleLeft } from "@fortawesome/free-regular-svg-icons";
@@ -29,7 +29,7 @@ interface Itab {
 const Tutorials = () => {
   const navigate = useNavigate();
   const [value, setValue] = useState(1);
-  
+
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     console.log(newValue);
     setValue(newValue);
@@ -72,28 +72,63 @@ const Tutorials = () => {
         />
       </div>
       <CustomTab fileName="로그인" index={0} value={value} file={login} />
-      <CustomTab fileName="전체 주식" index={1} value={value} file={stock_all} />
-      <CustomTab fileName="관심 주식" index={2} value={value} file={favourites} />
-      <CustomTab fileName="상세 주식" index={3} value={value} file={stock_detail} />
+      <CustomTab
+        fileName="전체 주식"
+        index={1}
+        value={value}
+        file={stock_all}
+      />
+      <CustomTab
+        fileName="관심 주식"
+        index={2}
+        value={value}
+        file={favourites}
+      />
+      <CustomTab
+        fileName="상세 주식"
+        index={3}
+        value={value}
+        file={stock_detail}
+      />
       <CustomTab fileName="매도 매수" index={4} value={value} file={buysell} />
       <CustomTab fileName="내 주식" index={5} value={value} file={my_stock} />
       <CustomTab fileName="내 거래" index={6} value={value} file={trade} />
       <CustomTab fileName="내 파산" index={7} value={value} file={bankrupt} />
-      <CustomTab fileName="데일리 퀴즈" index={8} value={value} file={daily_quiz} />
-      <CustomTab fileName="전체 랭킹" index={9} value={value} file={ranking_all} />
-      <CustomTab fileName="친구 랭킹" index={10} value={value} file={ranking_friend} />
+      <CustomTab
+        fileName="데일리 퀴즈"
+        index={8}
+        value={value}
+        file={daily_quiz}
+      />
+      <CustomTab
+        fileName="전체 랭킹"
+        index={9}
+        value={value}
+        file={ranking_all}
+      />
+      <CustomTab
+        fileName="친구 랭킹"
+        index={10}
+        value={value}
+        file={ranking_friend}
+      />
       <CustomTab fileName="챗봇" index={11} value={value} file={chatbot} />
     </div>
   );
-}
+};
 
 const CustomTab = (tab: Itab) => {
-  const {fileName, index, value, file} = tab;
-  if(value === index) {
+  const { fileName, index, value, file } = tab;
+  if (value === index) {
     return (
-    <div role="tabpanel" id={String(index)} className="w-full flex justify-center items-center h-[90%]">
-      <img src={file} alt={fileName} className="h-[100%]" />
-    </div>);
+      <div
+        role="tabpanel"
+        id={String(index)}
+        className="w-full flex justify-center items-center h-[90%]"
+      >
+        <img src={file} alt={fileName} className="h-[100%]" />
+      </div>
+    );
   } else {
     return null;
   }
