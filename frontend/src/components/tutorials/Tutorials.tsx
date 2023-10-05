001b4d13b191a53e34a075b188d55adb004f34fe
이예린 <h2002y@naver.com>
2023-10-05 15:13:11 +0900

    Feature/fe/tutorials

--- frontend/src/components/tutorials/Tutorials.tsx
+++ frontend/src/components/tutorials/Tutorials.tsx
@@ -54,6 +54,7 @@ const Tutorials = () => {
               <Tab label="전체 주식" />
               <Tab label="관심 주식" />
               <Tab label="상세 주식" />
+              <Tab label="매도 매수" />
               <Tab label="내 주식" />
               <Tab label="내 거래" />
               <Tab label="내 파산" />
@@ -74,13 +75,14 @@ const Tutorials = () => {
       <CustomTab fileName="전체 주식" index={1} value={value} file={stock_all} />
       <CustomTab fileName="관심 주식" index={2} value={value} file={favourites} />
       <CustomTab fileName="상세 주식" index={3} value={value} file={stock_detail} />
-      <CustomTab fileName="내 주식" index={4} value={value} file={my_stock} />
-      <CustomTab fileName="내 거래" index={5} value={value} file={trade} />
-      <CustomTab fileName="내 파산" index={6} value={value} file={bankrupt} />
-      <CustomTab fileName="데일리 퀴즈" index={7} value={value} file={daily_quiz} />
-      <CustomTab fileName="전체 랭킹" index={8} value={value} file={ranking_all} />
-      <CustomTab fileName="친구 랭킹" index={9} value={value} file={ranking_friend} />
-      <CustomTab fileName="챗봇" index={10} value={value} file={chatbot} />
+      <CustomTab fileName="매도 매수" index={4} value={value} file={buysell} />
+      <CustomTab fileName="내 주식" index={5} value={value} file={my_stock} />
+      <CustomTab fileName="내 거래" index={6} value={value} file={trade} />
+      <CustomTab fileName="내 파산" index={7} value={value} file={bankrupt} />
+      <CustomTab fileName="데일리 퀴즈" index={8} value={value} file={daily_quiz} />
+      <CustomTab fileName="전체 랭킹" index={9} value={value} file={ranking_all} />
+      <CustomTab fileName="친구 랭킹" index={10} value={value} file={ranking_friend} />
+      <CustomTab fileName="챗봇" index={11} value={value} file={chatbot} />
     </div>
   );
 }
